7ea4e4ed3ec70348b66d94426e8ce1eea1bdc89f
Duvonne Berry <8809406+duvonneb@users.noreply.github.com>
2025-01-27 14:56:54 -0500

    API fetch completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # duvonne-berry-open-api
+
+[My Open API Project] (https://github.com/duvonneb/duvonne-berry-open-api)
---
+++ js/index.js
@@ -0,0 +1,52 @@
+fetch(
+  "https://api.open-meteo.com/v1/forecast?latitude=28.5383&longitude=-81.3792&current=temperature_2m&daily=weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&models=best_match"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // Log the weather and weather code to the console
+    // Get the current temperature and weather code
+    const currentTemperature = data.current.temperature_2m;
+    const weatherCode = data.daily.weather_code[0];
+
+    // Convert weather code to description
+    const weatherDescription = weatherCodes[weatherCode];
+
+    // Log the information to the console
+    console.log(`Current Temperature in Orlando: ${currentTemperature}°F`);
+    console.log(`Weather: ${weatherDescription}`);
+  })
+  .catch((error) => {
+    console.error("Error fetching weather data:", error);
+  });
+
+//Weather codes descriptions
+const weatherCodes = {
+  0: "Clear sky",
+  1: "Mainly clear",
+  2: "Partly cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing rime fog",
+  51: "Light drizzle",
+  53: "Moderate drizzle",
+  55: "Heavy drizzle",
+  56: "Light freezing drizzle",
+  57: "Heavy freezing drizzle",
+  61: "Light rain",
+  63: "Moderate rain",
+  65: "Heavy rain",
+  66: "Light freezing rain",
+  67: "Heavy freezing rain",
+  71: "Light snow",
+  73: "Moderate snow",
+  75: "Heavy snow",
+  77: "Snow grains",
+  80: "Light rain showers",
+  81: "Moderate rain showers",
+  82: "Heavy rain showers",
+  85: "Light snow showers",
+  86: "Heavy snow showers",
+  95: "Thunderstorm",
+  96: "Thunderstorm with light hail",
+  99: "Thunderstorm with heavy hail",
+};
